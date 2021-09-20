3a0d4ad5966800996bbfe49e024e06b0870b980c
Yuichi Namiki <namiki081025@gmail.com>
2021-09-20 21:29:01 +0900

    first commit

---
+++ src/code01_01/Main.java
@@ -0,0 +1,11 @@
+package code01_01;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("RPG:スッキリ魔王討伐");
+		System.out.println("Ver.0.1 by 湊");
+		System.out.println("プログラムを終了します");
+	}
+
+}
